d12037fec7f3874bc4409babd7c7e92d2029309a
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-30 22:14:48 +0900

    style: 함수 주석 추가

--- utils.cpp
+++ utils.cpp
@@ -6,13 +6,26 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 21:40:08 by yena              #+#    #+#             */
-/*   Updated: 2023/10/30 21:45:08 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/30 22:14:22 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
-bool isNumber(char *str) {
+/**
+ * 에러 메시지를 출력하는 함수
+ * @param message 출력할 에러 메시지
+ */
+void printError(std::string message) {
+  std::cout << F_RED << "Error: " << message << FB_DEFAULT << std::endl;
+}
+
+/**
+ * 문자열이 숫자로만 이루어져있는지 확인하는 함수
+ * @param str 확인할 문자열
+ * @return 숫자로만 이루어져있으면 true, 아니면 false
+ */
+bool isNumber(const char *str) {
   int i = 0;
 
   while (str[i]) {
@@ -23,7 +36,12 @@ bool isNumber(char *str) {
   return true;
 }
 
-bool isValidPort(char *port) {
+/**
+ * 유효한 포트인지 확인하는 함수. 포트는 0 ~ 65535 사이의 숫자여야 한다.
+ * @param port 확인할 포트 문자열
+ * @return 유효한 포트면 true, 아니면 false
+ */
+bool isValidPort(const char *port) {
   if (!isNumber(port))
     return false;
 
