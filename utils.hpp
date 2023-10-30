3b361b964488ff4eb6e830a04a2f6fedb80442a4
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-30 22:15:24 +0900

    feat(utils): 에러 출력하는 함수 추가

--- utils.hpp
+++ utils.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 21:43:31 by yena              #+#    #+#             */
-/*   Updated: 2023/10/30 21:45:18 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/30 22:06:46 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 #include "common.hpp"
 
-bool isNumber(char *str);
-bool isValidPort(char *port);
+void printError(std::string message);
+bool isNumber(const char *str);
+bool isValidPort(const char *port);
 
 #endif //FT_IRC__UTILS_HPP_
