b1094b0d3aa6fc513306a667f6db764d5e53cc35
yena <50291995+nyj001012@users.noreply.github.com>
2023-11-01 14:14:55 +0900

    feat(server): select 사용하여 멀티플렉싱 서버 구현

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 22:23:09 by yena              #+#    #+#             */
-/*   Updated: 2023/11/01 13:36:58 by yena             ###   ########.fr       */
+/*   Updated: 2023/11/01 14:05:37 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,31 +22,32 @@
 
 #define MAX_CLIENT_NUM 10
 #define BUFFER_SIZE 512
+#define TIMEOUT_SEC 10
 
 class Server {
  public:
   Server();
-  Server(int max_client_number, int server_socket, fd_set client_fds);
   Server(const Server &server);
   Server &operator=(const Server &server);
   ~Server();
   int getMaxClientNumber() const;
   int getServerSocket() const;
   fd_set getClientFds() const;
+  int getFdMax() const;
   struct sockaddr_in getServerAddr() const;
   bool getIsDebug() const;
   void setMaxClientNumber(int max_client_number);
   void setServerSocket(int server_socket);
   void setClientFds(fd_set client_fds);
+  void setFdMax(int fd_max);
   void setServerAddr(struct sockaddr_in server_addr);
   void setIsDebug(bool is_debug);
   void initializeServer(const char *port);
-  void printServerInfo(struct sockaddr_in serv_addr);
   void initializeClient();
   void runServer();
   void acceptClient();
   void receiveMessage(int client_socket);
-  void sendMessage(int client_socket, std::string message);
+  void sendMessage(int client_socket, char *message);
   void closeClient(int client_socket);
   void closeServer();
 
@@ -54,6 +55,7 @@ class Server {
   int _max_client_number;
   int _server_socket;
   fd_set _client_fds;
+  int _fd_max;
   struct sockaddr_in _server_addr;
   bool _is_debug;
 };
--- server/Server.cpp
+++ server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 22:23:09 by yena              #+#    #+#             */
-/*   Updated: 2023/11/01 13:37:27 by yena             ###   ########.fr       */
+/*   Updated: 2023/11/01 14:11:31 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 Server::Server() {
   _max_client_number = MAX_CLIENT_NUM;
   _server_socket = 0;
+  _fd_max = 0;
   std::memset(&_server_addr, 0, sizeof(_server_addr));
   std::memset(&_client_fds, 0, sizeof(_client_fds));
 }
-
-Server::Server(int _max_client_number, int _server_socket, fd_set _client_fds) :
-    _max_client_number(_max_client_number), _server_socket(_server_socket), _client_fds(_client_fds) {}
 
 Server::Server(const Server &server) {
   if (this != &server) {
     _max_client_number = server._max_client_number;
     _server_socket = server._server_socket;
     _client_fds = server._client_fds;
+    _fd_max = server._fd_max;
+    _server_addr = server._server_addr;
+    _is_debug = server._is_debug;
   }
 }
 
@@ -35,12 +36,15 @@ Server &Server::operator=(const Server &server) {
     _max_client_number = server._max_client_number;
     _server_socket = server._server_socket;
     _client_fds = server._client_fds;
+    _fd_max = server._fd_max;
+    _server_addr = server._server_addr;
+    _is_debug = server._is_debug;
   }
   return *this;
 }
 
 Server::~Server() {
-  close(_server_socket);
+  this->closeServer();
 }
 
 int Server::getMaxClientNumber() const {
@@ -53,6 +57,10 @@ int Server::getServerSocket() const {
 
 fd_set Server::getClientFds() const {
   return this->_client_fds;
+}
+
+int Server::getFdMax() const {
+  return this->_fd_max;
 }
 
 struct sockaddr_in Server::getServerAddr() const {
@@ -75,6 +83,10 @@ void Server::setClientFds(fd_set client_fds) {
   this->_client_fds = client_fds;
 }
 
+void Server::setFdMax(int fd_max) {
+  this->_fd_max = fd_max;
+}
+
 void Server::setServerAddr(struct sockaddr_in server_addr) {
   this->_server_addr = server_addr;
 }
@@ -90,9 +102,9 @@ void Server::setIsDebug(bool is_debug) {
  */
 void Server::initializeServer(const char *port) {
   struct sockaddr_in serv_addr;
-  int server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  this->_server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-  if (server_socket == -1)
+  if (this->_server_socket == -1)
     throw std::runtime_error("Error: socket() failed");
   char *port_backup = new char[std::strlen(port) + 1];
   std::strcpy(port_backup, port);
@@ -101,27 +113,18 @@ void Server::initializeServer(const char *port) {
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   serv_addr.sin_port = htons(std::atoi(port_backup));
 
-  if (bind(server_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
-    close(server_socket);
+  if (bind(this->_server_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
+    this->closeServer();
     throw std::runtime_error("Error: bind() failed");
   }
-  if (this->getIsDebug())
-    printServerInfo(serv_addr);
+  if (this->_is_debug)
+    std::cout << F_YELLOW << "[DEBUG] Server initialized: " << FB_DEFAULT << std::endl;
   delete[] port_backup;
-  if (listen(server_socket, MAX_CLIENT_NUM) == -1) {
-    close(server_socket);
+  if (listen(this->_server_socket, MAX_CLIENT_NUM) == -1) {
+    this->closeServer();
     throw std::runtime_error("Error: listen() failed");
   }
-  this->setServerSocket(server_socket);
-  this->setServerAddr(serv_addr);
-}
-
-/**
- * 서버의 정보를 출력한다.
- * @param _server_socket 서버 소켓 fd값
- * @param serv_addr
- */
-void Server::printServerInfo(struct sockaddr_in serv_addr) {
+  this->_server_addr = serv_addr;
 }
 
 /**
@@ -130,44 +133,95 @@ void Server::printServerInfo(struct sockaddr_in serv_addr) {
 void Server::initializeClient() {
   struct sockaddr_in addr_client;
   socklen_t addr_client_len = sizeof(addr_client_len);
+  fd_set client_fds = this->getClientFds();
+  int serverSocket = this->getServerSocket();
 
-  std::memset(&addr_client, 0, sizeof(addr_client));
-  client_socket = accept(_server_socket, (struct sockaddr *) &addr_client, &addr_client_len);
-  if (client_socket == -1) {
-    close(_server_socket);
-    throw std::runtime_error("Error: accept() failed");
-  }
+  FD_ZERO(&client_fds);
+  FD_SET(serverSocket, &client_fds);
+  this->setFdMax(serverSocket);
 }
 
 /**
  * 서버를 실행한다. 클라이언트로부터 명령어를 받아서 처리한다.
  */
 void Server::runServer() {
-  char buffer[BUFFER_SIZE];
+  struct timeval tv = {TIMEOUT_SEC, 0};
 
   while (true) {
-    std::memset(buffer, 0, BUFFER_SIZE);
-    int read_size = read(client_socket, buffer, sizeof(buffer) - 2);
-    if (read_size == -1) {
-      std::cerr << "read() error" << std::endl;
-      break;
+    if (this->_is_debug) {
+      std::cout << F_YELLOW << "[DEBUG] Server running..." << FB_DEFAULT << std::endl;
+      std::cout << *this << std::endl;
     }
-    std::strcat(buffer, "\r\n");
-    if (is_debug)
-      std::cout << F_CYAN << "[ INFO ] " << FB_DEFAULT << "Client:\n" << buffer << std::endl;
-    // TODO => 명령어 파싱
-    // TODO => 명령어 실행하는 함수에 파싱된 명령어 전달
+    int ready_descriptors = select(this->_fd_max + 1, &this->_client_fds, nullptr, nullptr, &tv);
+    if (ready_descriptors == -1)
+      throw std::runtime_error("Error: select() failed");
+    if (ready_descriptors == 0)
+      continue;
+    for (int i = 0; i <= this->_fd_max; i++) {
+      if (FD_ISSET(i, &this->_client_fds)) {
+        if (i == this->getServerSocket())
+          this->acceptClient();
+        else
+          this->receiveMessage(i);
+      }
+    }
   }
-  close(_server_socket);
+}
+
+void Server::acceptClient() {
+  socklen_t client_addr_len = sizeof(struct sockaddr_in);
+  struct sockaddr client_addr;
+  int client_socket = accept(this->getServerSocket(), &client_addr, &client_addr_len);
+  if (client_socket == -1)
+    throw std::runtime_error("Error: accept() failed");
+  FD_SET(client_socket, &this->_client_fds);
+  if (client_socket > this->_fd_max)
+    this->_fd_max = client_socket;
+  if (this->_is_debug)
+    std::cout << F_YELLOW << "[DEBUG] New client connected: " << client_socket << FB_DEFAULT << std::endl;
+}
+
+void Server::receiveMessage(int client_socket) {
+  char buffer[BUFFER_SIZE];
+
+  std::memset(buffer, 0, BUFFER_SIZE);
+  ssize_t read_size = read(client_socket, buffer, BUFFER_SIZE);
+  if (read_size == -1)
+    throw std::runtime_error("Error: read() failed");
+  if (read_size == 0) {
+    this->closeClient(client_socket);
+    return;
+  }
+  if (this->_is_debug)
+    std::cout << F_YELLOW << "[DEBUG] Message received: " << buffer << FB_DEFAULT << std::endl;
+  this->sendMessage(client_socket, buffer);
+}
+
+void Server::closeClient(int client_socket) {
+  close(client_socket);
+  FD_CLR(client_socket, &this->_client_fds);
+  if (this->_is_debug)
+    std::cout << F_YELLOW << "[DEBUG] Client closed: " << client_socket << FB_DEFAULT << std::endl;
+}
+
+void Server::sendMessage(int client_socket, char *message) {
+  ssize_t write_size = write(client_socket, message, std::strlen(message));
+  if (write_size == -1)
+    throw std::runtime_error("Error: write() failed");
+}
+
+void Server::closeServer() {
+  close(this->_server_socket);
 }
 
 std::ostream &operator<<(std::ostream &os, const fd_set &client_fds) {
   os << "[ ";
   for (int i = 0; i < FD_SETSIZE; i++) {
-    if (FD_ISSET(i, &client_fds))
+    if (FD_ISSET(i, &client_fds)) {
       os << i << std::endl;
-    if (i != FD_SETSIZE - 1)
-      os << ", ";
+      if (i != FD_SETSIZE - 1)
+        os << ", ";
+    }
   }
   os << " ]";
   return os;
@@ -176,7 +230,7 @@ std::ostream &operator<<(std::ostream &os, const fd_set &client_fds) {
 std::ostream &operator<<(std::ostream &os, const Server &server) {
   struct sockaddr_in server_addr = server.getServerAddr();
 
-  os << F_CYAN << "[INFO] Server: " << FB_DEFAULT << std::endl;
+  os << F_YELLOW << "[DEBUG] Server: " << FB_DEFAULT << std::endl;
   os << "=> Server socket    : " << server.getServerSocket() << std::endl;
   os << "=> Server address   : " << inet_ntoa(server_addr.sin_addr) << std::endl;
   os << "=> Server port      : " << ntohs(server_addr.sin_port) << std::endl;
