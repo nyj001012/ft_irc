387c744392ef183ed4aba36aa9acc18be4a6491a
yena <50291995+nyj001012@users.noreply.github.com>
2023-11-07 13:17:52 +0900

    style(parse): 헤더에 기능 별 구분하는 주석 추가

--- include/parse.hpp
+++ include/parse.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 13:43:05 by yena              #+#    #+#             */
-/*   Updated: 2023/11/06 21:13:19 by yena             ###   ########.fr       */
+/*   Updated: 2023/11/07 12:23:44 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,32 @@
 #include "utils.hpp"
 #include <iomanip>
 
+/*----------------------------------*
+ * Return Value                     *
+ *----------------------------------*/
 #define SUCCESS true
 #define FAIL false
+
+/*----------------------------------*
+ * Token Type                       *
+ *----------------------------------*/
 #define SERVER_NAME_OR_NICK "server_name | nick"
 #define USER "user"
 #define HOST "host"
 #define COMMAND "command"
 #define PARAMS "params"
 
+/*----------------------------------*
+ * Token Structure                  *
+ *----------------------------------*/
 typedef struct s_token {
   std::string type;
   std::string value;
 } t_token;
 
+/*----------------------------------*
+ * Functions                        *
+ *----------------------------------*/
 int parseMessageFormat(std::string command, bool is_debug, std::vector<t_token> &tokens);
 int parseUserAndHost(std::string user_and_host, std::vector<t_token> &tokens);
 int parseCommandWithOptions(std::string command, std::vector<t_token> &tokens);
