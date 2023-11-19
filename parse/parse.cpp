53a1449a341fd509497e9d72c31cd2b86bd28eff
yena <50291995+nyj001012@users.noreply.github.com>
2023-11-19 15:16:25 +0900

    style(yena): 코드 스타일 microsoft로 변경 (#28)

--- parse/parse.cpp
+++ parse/parse.cpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 13:43:05 by yena              #+#    #+#             */
-/*   Updated: 2023/11/19 15:03:12 by yena             ###   ########.fr       */
+/*   Updated: 2023/11/19 15:13:42 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,20 +36,21 @@ or NUL or CR or LF, the first of which may not be ':'>
  * @param tokens 파싱된 토큰들이 저장될 벡터
  * @return 유효한 명령어면 SUCCESS, 아니면 FAIL
  */
-int parseMessageFormat(std::string command, bool is_debug, std::vector<t_token> &tokens) {
-  bool result;
-  if (command.empty() || command[command.length() - 1] != '\n')
-    result = FAIL;
-  command.erase(command.length() - 1); // '\n' 제거
-  command.erase(command.length() - 2); // '\r' 제거
-  if (command[0] == ':')
-    result = parseCommandWithOptions(command, tokens);
-  else
-    result = parseCommand(command, tokens);
-  reorderTrailing(tokens);
-  if (is_debug)
-    printTokens(tokens);
-  return result;
+int parseMessageFormat(std::string command, bool is_debug, std::vector<t_token>& tokens)
+{
+	bool result;
+	if (command.empty() || command[command.length() - 1] != '\n')
+		result = FAIL;
+	command.erase(command.length() - 1); // '\n' 제거
+	command.erase(command.length() - 2); // '\r' 제거
+	if (command[0] == ':')
+		result = parseCommandWithOptions(command, tokens);
+	else
+		result = parseCommand(command, tokens);
+	reorderTrailing(tokens);
+	if (is_debug)
+		printTokens(tokens);
+	return result;
 }
 
 /**
@@ -59,18 +60,20 @@ int parseMessageFormat(std::string command, bool is_debug, std::vector<t_token> 
  * @param tokens 파싱된 토큰들이 저장될 벡터
  * @return 유효한 options면 SUCCESS, 아니면 FAIL
  */
-int parseCommandWithOptions(std::string command, std::vector<t_token> &tokens) {
-  size_t pos = command.find(' ');
-  if (pos == std::string::npos)
-    return FAIL;
-  std::string options = command.substr(1, pos - 1);
-  if (options.empty())
-    return FAIL;
-  if (!parseUserAndHost(options, tokens)) {
-    return FAIL;
-  }
-  skipChar(command, ' ');
-  return parseCommand(command, tokens);
+int parseCommandWithOptions(std::string command, std::vector<t_token>& tokens)
+{
+	size_t pos = command.find(' ');
+	if (pos == std::string::npos)
+		return FAIL;
+	std::string options = command.substr(1, pos - 1);
+	if (options.empty())
+		return FAIL;
+	if (!parseUserAndHost(options, tokens))
+	{
+		return FAIL;
+	}
+	skipChar(command, ' ');
+	return parseCommand(command, tokens);
 }
 
 /**
@@ -80,28 +83,31 @@ int parseCommandWithOptions(std::string command, std::vector<t_token> &tokens) {
  * @param tokens 파싱된 토큰들이 저장될 벡터
  * @return 유효한 user와 host면 SUCCESS, 아니면 FAIL
  */
-int parseUserAndHost(std::string user_and_host, std::vector<t_token> &tokens) {
-  size_t user_pos = user_and_host.find('!');
-  size_t host_pos = user_and_host.find('@');
-  if (user_pos != std::string::npos) { // user가 있는 경우
-    if (user_pos == 0 || user_pos == user_and_host.length() - 1)
-      return FAIL;
-    std::string user = user_and_host.substr(user_pos + 1);
-    if (host_pos != std::string::npos) // user와 host 모두 있는 경우
-      user = user.substr(0, host_pos - user_pos - 1);
-    tokens.push_back((t_token) {NICK, user_and_host.substr(0, user_pos)});
-    tokens.push_back((t_token) {USER, user});
-  }
-  if (host_pos != std::string::npos) { // host가 있는 경우
-    if (host_pos == 0 || host_pos == user_and_host.length() - 1)
-      return FAIL;
-    if (user_pos == std::string::npos) // user 없이 host만 있는 경우
-      tokens.push_back((t_token) {NICK, user_and_host.substr(0, host_pos)});
-    tokens.push_back((t_token) {HOST, user_and_host.substr(host_pos + 1)});
-  }
-  if (user_pos == std::string::npos && host_pos == std::string::npos) // user, host가 없는 경우
-    tokens.insert(tokens.begin(), (t_token) {NICK, user_and_host});
-  return SUCCESS;
+int parseUserAndHost(std::string user_and_host, std::vector<t_token>& tokens)
+{
+	size_t user_pos = user_and_host.find('!');
+	size_t host_pos = user_and_host.find('@');
+	if (user_pos != std::string::npos)
+	{ // user가 있는 경우
+		if (user_pos == 0 || user_pos == user_and_host.length() - 1)
+			return FAIL;
+		std::string user = user_and_host.substr(user_pos + 1);
+		if (host_pos != std::string::npos) // user와 host 모두 있는 경우
+			user = user.substr(0, host_pos - user_pos - 1);
+		tokens.push_back((t_token){ NICK, user_and_host.substr(0, user_pos) });
+		tokens.push_back((t_token){ USER, user });
+	}
+	if (host_pos != std::string::npos)
+	{ // host가 있는 경우
+		if (host_pos == 0 || host_pos == user_and_host.length() - 1)
+			return FAIL;
+		if (user_pos == std::string::npos) // user 없이 host만 있는 경우
+			tokens.push_back((t_token){ NICK, user_and_host.substr(0, host_pos) });
+		tokens.push_back((t_token){ HOST, user_and_host.substr(host_pos + 1) });
+	}
+	if (user_pos == std::string::npos && host_pos == std::string::npos) // user, host가 없는 경우
+		tokens.insert(tokens.begin(), (t_token){ NICK, user_and_host });
+	return SUCCESS;
 }
 
 /**
@@ -111,25 +117,31 @@ int parseUserAndHost(std::string user_and_host, std::vector<t_token> &tokens) {
  * @param tokens 파싱된 토큰들이 저장될 벡터
  * @return 유효한 command면 SUCCESS, 아니면 FAIL
  */
-int parseCommand(std::string command_part, std::vector<t_token> &tokens) {
-  if (command_part.empty())
-    return FAIL;
-  std::string command = command_part.substr(0, command_part.find(' '));
-  if (std::isdigit(command[0])) {
-    if (command.length() != 3)
-      return FAIL;
-    for (size_t i = 0; i < 3; i++) {
-      if (!std::isdigit(command[i]))
-        return FAIL;
-    }
-  } else {
-    for (size_t i = 0; i < command.length(); i++) {
-      if (!std::isalpha(command[i]))
-        return FAIL;
-    }
-  }
-  tokens.push_back((t_token) {COMMAND, command});
-  return parseParams(command_part, tokens);
+int parseCommand(std::string command_part, std::vector<t_token>& tokens)
+{
+	if (command_part.empty())
+		return FAIL;
+	std::string command = command_part.substr(0, command_part.find(' '));
+	if (std::isdigit(command[0]))
+	{
+		if (command.length() != 3)
+			return FAIL;
+		for (size_t i = 0; i < 3; i++)
+		{
+			if (!std::isdigit(command[i]))
+				return FAIL;
+		}
+	}
+	else
+	{
+		for (size_t i = 0; i < command.length(); i++)
+		{
+			if (!std::isalpha(command[i]))
+				return FAIL;
+		}
+	}
+	tokens.push_back((t_token){ COMMAND, command });
+	return parseParams(command_part, tokens);
 }
 
 /**
@@ -139,17 +151,21 @@ int parseCommand(std::string command_part, std::vector<t_token> &tokens) {
  * @param tokens 파싱된 토큰들이 저장될 벡터
  * @return 유효한 파라미터면 SUCCESS, 아니면 FAIL
  */
-int parseParams(std::string command_part, std::vector<t_token> &tokens) {
-  if (command_part.find(' ') == std::string::npos)
-    return SUCCESS;
-  skipChar(command_part, ' ');
-  if (command_part.empty())
-    return FAIL;
-  if (command_part[0] == ':') {
-    return parseTrailing(command_part, tokens);
-  } else {
-    return parseMiddle(command_part, tokens);
-  }
+int parseParams(std::string command_part, std::vector<t_token>& tokens)
+{
+	if (command_part.find(' ') == std::string::npos)
+		return SUCCESS;
+	skipChar(command_part, ' ');
+	if (command_part.empty())
+		return FAIL;
+	if (command_part[0] == ':')
+	{
+		return parseTrailing(command_part, tokens);
+	}
+	else
+	{
+		return parseMiddle(command_part, tokens);
+	}
 }
 
 /**
@@ -160,13 +176,15 @@ int parseParams(std::string command_part, std::vector<t_token> &tokens) {
  * @param tokens 파싱된 토큰들이 저장될 벡터
  * @return 유효한 trailing이면 SUCCESS, 아니면 FAIL
  */
-int parseTrailing(std::string params, std::vector<t_token> &tokens) {
-  for (size_t i = 1; i < params.length(); i++) {
-    if (params[i] == '\0' || params[i] == '\r' || params[i] == '\n')
-      return FAIL;
-  }
-  tokens.push_back((t_token) {TRAILING, params.substr(1)});
-  return SUCCESS;
+int parseTrailing(std::string params, std::vector<t_token>& tokens)
+{
+	for (size_t i = 1; i < params.length(); i++)
+	{
+		if (params[i] == '\0' || params[i] == '\r' || params[i] == '\n')
+			return FAIL;
+	}
+	tokens.push_back((t_token){ TRAILING, params.substr(1) });
+	return SUCCESS;
 }
 
 /**
@@ -177,51 +195,58 @@ int parseTrailing(std::string params, std::vector<t_token> &tokens) {
  * @param tokens 파싱된 토큰들이 저장될 벡터
  * @return 유효한 middle이면 SUCCESS, 아니면 FAIL
  */
-int parseMiddle(std::string params, std::vector<t_token> &tokens) {
-  if (params.empty() || params[0] == ':')
-    return FAIL;
-  for (size_t i = 0; i < params.length(); i++) {
-    if (params[i] == ' ')
-      return parseParams(params.substr(i), tokens);
-    if (params[i] == '\0' || params[i] == '\r' || params[i] == '\n' || params[i] == ' ')
-      return FAIL;
-  }
-  tokens.push_back((t_token) {MIDDLE, params});
-  return SUCCESS;
+int parseMiddle(std::string params, std::vector<t_token>& tokens)
+{
+	if (params.empty() || params[0] == ':')
+		return FAIL;
+	for (size_t i = 0; i < params.length(); i++)
+	{
+		if (params[i] == ' ')
+			return parseParams(params.substr(i), tokens);
+		if (params[i] == '\0' || params[i] == '\r' || params[i] == '\n' || params[i] == ' ')
+			return FAIL;
+	}
+	tokens.push_back((t_token){ MIDDLE, params });
+	return SUCCESS;
 }
 
 /**
  * trailing이 middle보다 앞에 오도록 순서를 바꾸는 함수
  * @param tokens 파싱된 토큰들이 저장된 벡터
  */
-void reorderTrailing(std::vector<t_token> &tokens) {
-  for (size_t i = 0; i < tokens.size(); i++) {
-    if (tokens[i].type == TRAILING) {
-      t_token trailing = tokens[i];
-      tokens.erase(tokens.begin() + i);
-      tokens.push_back(trailing);
-      break;
-    }
-  }
+void reorderTrailing(std::vector<t_token>& tokens)
+{
+	for (size_t i = 0; i < tokens.size(); i++)
+	{
+		if (tokens[i].type == TRAILING)
+		{
+			t_token trailing = tokens[i];
+			tokens.erase(tokens.begin() + i);
+			tokens.push_back(trailing);
+			break;
+		}
+	}
 }
 
 /**
  * 파싱된 토큰들을 출력하는 함수
  * @param tokens 파싱된 토큰들이 저장된 벡터
  */
-void printTokens(std::vector<t_token> tokens) {
-  std::cout << F_YELLOW << "[DEBUG] Tokens: " << FB_DEFAULT << std::endl;
-  size_t type_length = getLongestTokenType(tokens);
-  size_t value_length = getLongestTokenValue(tokens);
+void printTokens(std::vector<t_token> tokens)
+{
+	std::cout << F_YELLOW << "[DEBUG] Tokens: " << FB_DEFAULT << std::endl;
+	size_t type_length = getLongestTokenType(tokens);
+	size_t value_length = getLongestTokenValue(tokens);
 
-  type_length = type_length > 4 ? type_length : 4;
-  value_length = value_length > 5 ? value_length : 5;
-  std::cout << F_BLACK_W_WHITE << "| " << std::setw(type_length) << "TYPE" << " | ";
-  std::cout << std::setw(value_length) << "VALUE" << " |" << FB_DEFAULT << std::endl;
-  for (size_t i = 0; i < tokens.size(); i++) {
-    std::cout << "| " << std::setw(type_length) << tokens[i].type << " | ";
-    std::cout << std::setw(value_length) << tokens[i].value << " |" << std::endl;
-  }
+	type_length = type_length > 4 ? type_length : 4;
+	value_length = value_length > 5 ? value_length : 5;
+	std::cout << F_BLACK_W_WHITE << "| " << std::setw(type_length) << "TYPE" << " | ";
+	std::cout << std::setw(value_length) << "VALUE" << " |" << FB_DEFAULT << std::endl;
+	for (size_t i = 0; i < tokens.size(); i++)
+	{
+		std::cout << "| " << std::setw(type_length) << tokens[i].type << " | ";
+		std::cout << std::setw(value_length) << tokens[i].value << " |" << std::endl;
+	}
 }
 
 /**
@@ -229,13 +254,15 @@ void printTokens(std::vector<t_token> tokens) {
  * @param tokens 토큰들이 저장된 벡터
  * @return 가장 긴 type의 길이
  */
-size_t getLongestTokenType(std::vector<t_token> tokens) {
-  size_t type_length = 0;
-  for (size_t i = 0; i < tokens.size(); i++) {
-    if (tokens[i].type.length() > type_length)
-      type_length = tokens[i].type.length();
-  }
-  return type_length;
+size_t getLongestTokenType(std::vector<t_token> tokens)
+{
+	size_t type_length = 0;
+	for (size_t i = 0; i < tokens.size(); i++)
+	{
+		if (tokens[i].type.length() > type_length)
+			type_length = tokens[i].type.length();
+	}
+	return type_length;
 }
 
 /**
@@ -243,11 +270,13 @@ size_t getLongestTokenType(std::vector<t_token> tokens) {
  * @param tokens 토큰들이 저장된 벡터
  * @return 가장 긴 value의 길이
  */
-size_t getLongestTokenValue(std::vector<t_token> tokens) {
-  size_t value_length = 0;
-  for (size_t i = 0; i < tokens.size(); i++) {
-    if (tokens[i].value.length() > value_length)
-      value_length = tokens[i].value.length();
-  }
-  return value_length;
+size_t getLongestTokenValue(std::vector<t_token> tokens)
+{
+	size_t value_length = 0;
+	for (size_t i = 0; i < tokens.size(); i++)
+	{
+		if (tokens[i].value.length() > value_length)
+			value_length = tokens[i].value.length();
+	}
+	return value_length;
 }
--- server/Server.hpp
+++ server/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 22:23:09 by yena              #+#    #+#             */
-/*   Updated: 2023/11/19 15:04:52 by yena             ###   ########.fr       */
+/*   Updated: 2023/11/19 15:13:37 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,43 +26,44 @@
 #define BUFFER_SIZE 512
 #define TIMEOUT_SEC 5
 
-class Server {
- public:
-  Server();
-  Server(const Server &server);
-  Server &operator=(const Server &server);
-  ~Server();
-  int getMaxClientNumber() const;
-  int getServerSocket() const;
-  fd_set getClientFds() const;
-  int getFdMax() const;
-  struct sockaddr_in getServerAddr() const;
-  bool getIsDebug() const;
-  void setMaxClientNumber(int max_client_number);
-  void setServerSocket(int server_socket);
-  void setClientFds(fd_set client_fds);
-  void setFdMax(int fd_max);
-  void setServerAddr(struct sockaddr_in server_addr);
-  void setIsDebug(bool is_debug);
-  void initializeServer(const char *port);
-  void initializeClientFds();
-  void runServer();
-  void acceptClient();
-  std::string receiveMessage(int client_socket);
-  void sendMessage(int client_socket, std::string message);
-  void closeClient(int client_socket);
-  void closeServer();
+class Server
+{
+public:
+	Server();
+	Server(const Server& server);
+	Server& operator=(const Server& server);
+	~Server();
+	int getMaxClientNumber() const;
+	int getServerSocket() const;
+	fd_set getClientFds() const;
+	int getFdMax() const;
+	struct sockaddr_in getServerAddr() const;
+	bool getIsDebug() const;
+	void setMaxClientNumber(int max_client_number);
+	void setServerSocket(int server_socket);
+	void setClientFds(fd_set client_fds);
+	void setFdMax(int fd_max);
+	void setServerAddr(struct sockaddr_in server_addr);
+	void setIsDebug(bool is_debug);
+	void initializeServer(const char* port);
+	void initializeClientFds();
+	void runServer();
+	void acceptClient();
+	std::string receiveMessage(int client_socket);
+	void sendMessage(int client_socket, std::string message);
+	void closeClient(int client_socket);
+	void closeServer();
 
- private:
-  int _max_client_number;
-  int _server_socket;
-  fd_set _client_fds;
-  int _fd_max;
-  struct sockaddr_in _server_addr;
-  bool _is_debug;
+private:
+	int _max_client_number;
+	int _server_socket;
+	fd_set _client_fds;
+	int _fd_max;
+	struct sockaddr_in _server_addr;
+	bool _is_debug;
 };
 
-std::ostream &operator<<(std::ostream &os, const Server &server);
-std::ostream &operator<<(std::ostream &os, const fd_set &client_fds);
+std::ostream& operator<<(std::ostream& os, const Server& server);
+std::ostream& operator<<(std::ostream& os, const fd_set& client_fds);
 
 #endif //FT_IRC_SERVER_INITIALIZE_HPP_
