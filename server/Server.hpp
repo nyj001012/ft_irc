a10437417d840d3ccf3d7c12caffdc937f6b1b42
yena <50291995+nyj001012@users.noreply.github.com>
2023-11-22 14:55:18 +0900

    debug 모드 진입 불가 (#34)
    * style(server): 코드 인덴트 변경
    
    * fix(main): argument if문 조건 변경

--- server/Server.hpp
+++ server/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 22:23:09 by yena              #+#    #+#             */
-/*   Updated: 2023/11/19 15:13:37 by yena             ###   ########.fr       */
+/*   Updated: 2023/11/22 13:37:53 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,40 +27,41 @@
 #define BUFFER_SIZE 512
 #define TIMEOUT_SEC 5
 
-class Server {
- public:
-  Server();
-  Server(const Server &server);
-  Server &operator=(const Server &server);
-  ~Server();
-  int getMaxClientNumber() const;
-  int getServerSocket() const;
-  fd_set getClientFds() const;
-  int getFdMax() const;
-  struct sockaddr_in getServerAddr() const;
-  bool getIsDebug() const;
-  void setMaxClientNumber(int max_client_number);
-  void setServerSocket(int server_socket);
-  void setClientFds(fd_set client_fds);
-  void setFdMax(int fd_max);
-  void setServerAddr(struct sockaddr_in server_addr);
-  void setIsDebug(bool is_debug);
-  void initializeServer(const char *port);
-  void initializeClientFds();
-  void runServer();
-  void acceptClient();
+class Server
+{
+public:
+	Server();
+	Server(const Server& server);
+	Server& operator=(const Server& server);
+	~Server();
+	int getMaxClientNumber() const;
+	int getServerSocket() const;
+	fd_set getClientFds() const;
+	int getFdMax() const;
+	struct sockaddr_in getServerAddr() const;
+	bool getIsDebug() const;
+	void setMaxClientNumber(int max_client_number);
+	void setServerSocket(int server_socket);
+	void setClientFds(fd_set client_fds);
+	void setFdMax(int fd_max);
+	void setServerAddr(struct sockaddr_in server_addr);
+	void setIsDebug(bool is_debug);
+	void initializeServer(const char* port);
+	void initializeClientFds();
+	void runServer();
+	void acceptClient();
 	std::string receiveMessage(int client_socket);
-  void sendMessage(int client_socket, std::string& message);
-  void closeClient(int client_socket);
-  void closeServer();
+	void sendMessage(int client_socket, std::string& message);
+	void closeClient(int client_socket);
+	void closeServer();
 
- private:
-  int _max_client_number;
-  int _server_socket;
-  fd_set _client_fds;
-  int _fd_max;
-  struct sockaddr_in _server_addr;
-  bool _is_debug;
+private:
+	int _max_client_number;
+	int _server_socket;
+	fd_set _client_fds;
+	int _fd_max;
+	struct sockaddr_in _server_addr;
+	bool _is_debug;
 	RequestHandler handler;
 };
 std::ostream& operator<<(std::ostream& os, const Server& server);
